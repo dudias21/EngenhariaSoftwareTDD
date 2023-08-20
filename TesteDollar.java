07beca702d0e25b3cc5bd10869de7271a46dfa40
dudias21 <134941853+dudias21@users.noreply.github.com>
2023-08-20 16:29:56 +0000

    Parte 2 implementada

--- TesteDollar.java
+++ TesteDollar.java
@@ -5,14 +5,17 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-class TesteDollar{
-@Test
-public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
-    }
+public class TesteDollar {
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+  }
 
-
-
+  @Test
+  public void testEquality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+  }
 }
