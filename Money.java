5ffca1b77e77458eb5b6b7f02e52f776ed6fd5b4
dudias21 <134941853+dudias21@users.noreply.github.com>
2023-08-20 18:14:52 +0000

    Rtemoção equality e franc realizada

--- Money.java
+++ Money.java
@@ -1,16 +1,24 @@
 abstract class Money {
-   private String currency; 
+  private String currency;
 
-   static Money dollar(int amount)  {
-      return new Dollar(amount, "USD");
-   }
+  static Money dollar(int amount) {
+    return new Money(amount, "USD");
+  }
 
-   static Money franc(int amount) {
-      return new Franc(amount, "CHF");
-   }
+  static Money franc(int amount) {
+    return new Money(amount, "CHF");
+  }
 
-   Money(int amount, String currency) {
-      this.amount = amount;
-      this.currency = currency;
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return amount == money.amount && currency().equals(money.currency());
+  }
+
+  Money times(int multiplier) {
+      return new Money(amount * multiplier, currency);
    }
-}
